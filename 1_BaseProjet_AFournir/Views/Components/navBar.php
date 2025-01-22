fda10a627492da7f8321502e6b6b479539649e53
Blaze Girboux <230399@site.asty-moulin.be>
2025-01-22 15:49:16 +0100

    ajout de l'étape une

---
+++ 1_BaseProjet_AFournir/Views/Components/navBar.php
@@ -0,0 +1,9 @@
+<ul class="flexible space-evenly">
+            <!-- grand écran -->
+            <li class="menu"><a href="index.php">Home</a></li>
+            <li  class="menu"><a href="/inscription">Inscription</a></li>
+            <li  class="menu"><a href="/connexion">Connexion</a></li>
+            <!-- petit écran -->
+            <li class="imageMenu"><a href="index.php"><ion-icon size="large" name="home-outline"></ion-icon></a></li>
+            <li class="imageMenu"><a href="/insciption"><ion-icon size="large" name="person-outline"></ion-icon></a></li>
+        </ul>
---
+++ 1_BaseProjet_AFournir/Views/pageAccueil.php
@@ -0,0 +1,15 @@
+<h1>Liste des écoles répertoriées</h1>
+
+<div class="Flexible wrap space-around">
+    <div class="border card">
+        <h2 class="center" >ITN</h2>
+        <div>
+            <div class="flexible blocImageEcole"><img src="../Assets/Images/itnIm.jpeg" alt="itn"></div>
+            <div class="center">
+                <p><span>Rue de la pépinière 101</span> - <span>Namur</span></p>
+                <h3>081729011</h3>
+                <a href="voirEcole.php" class="btn btn-page">Voir l'école</a>
+            </div>
+        </div>
+    </div>
+</div>
